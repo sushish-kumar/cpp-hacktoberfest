80aeaaf768dc486a3b5d78b87b070f16154679e0
12345radha <56878565+12345radha@users.noreply.github.com>
2020-10-15 20:53:23 +0530

    Create Radha-Kumari.cpp

---
+++ subtraction/Radha-Kumari.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,second_number;
+    int difference;
+    cin>> first_number >> second_number;
+    difference = first_number - second_number;
+    cout << "Difference = " << difference << endl;
+    return 0;
+}
