bddf27b34960db2afff34d4e7c6456e20632b29b
12345radha <56878565+12345radha@users.noreply.github.com>
2020-10-15 20:44:19 +0530

    Create Radha Kumari.cpp

---
+++ addition/Radha Kumari.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int first_number,second_number;
+    int addition;
+    cin>> first_number >> second_number ;
+    addition = first_number + second_number;
+    cout << "Addition = " << addition << endl;
+    return 0;
+}
