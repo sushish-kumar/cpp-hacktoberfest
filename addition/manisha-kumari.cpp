f20a8d706f5f6ed506b9c063652358d9521629d3
Manisha Kumari <krimanisha666@gmail.com>
2020-10-15 09:22:55 +0530

    Add addition, hello world, substraction

---
+++ addition/manisha-kumari.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int sum;
+    cin>> a >> b;
+    sum = a + b;
+    cout << "Sum = " << sum << endl;
+    return 0;
+}
---
+++ subtraction/manisha-kumari.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    int diff;
+    cin>> a >> b;
+    diff = a - b;
+    cout << "Difference = " << diff << endl;
+    return 0;
+}
