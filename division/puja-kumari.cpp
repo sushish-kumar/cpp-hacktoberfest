4d1fb30413b39020165b87fb15d90b2b11b6d956
Puja Kumari <rajaradhya7260@gmail.com>
2020-10-17 15:34:19 +0530

    Add cpp

---
+++ division/puja-kumari.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int first_number,second_number,quotient;
+  cin>>first_number>>second_number;
+  quotient = first_number/second_number;
+  cout<<quotient<<endl;
+}
