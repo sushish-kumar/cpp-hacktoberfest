8f64854050b406317687dd050d7a2c1e796e99b8
Vaibhav Goel <87615534+vaibhav2809@users.noreply.github.com>
2021-10-02 19:14:06 +0530

    Create vaibhav2809.cpp

---
+++ addition/vaibhav2809.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter two numbers :";
+    int n,m;
+    cin>>n>>m;
+    cout<<"Sum of the two numbers is : "<<n+m;
+
+    return 0;
+}
