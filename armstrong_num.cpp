f508e35007d629a5efffb9db64f27bd0ab7d5374
Sushant Barje <sushantbarje4@gmail.com>
2021-10-09 10:22:54 +0530

    Generalized to get it working for numbers of any digits

--- armstrong_num.cpp
+++ armstrong_num.cpp
@@ -1,30 +1,37 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int n,rem,sum=0,temp;
-    cin>>n;
+    //First take input as string to know the number of digits in number.
+    string n;
+    int rem, sum = 0, temp;
+    cin >> n;
 
-    temp = n;
+    //store length of string i.e. no of digits
+    int no_of_digits = n.length();
 
-    while (n != 0)
+    //type cast it to interger.
+    int num = stoi(n);
+    temp = num;
+
+    while (num != 0)
     {
-        rem = n%10;
-        sum = sum + pow(rem,3);
-        n = n/10;
+        rem = num % 10;
+        sum = sum + pow(rem, no_of_digits);
+        num = num / 10;
     }
 
     if (temp == sum)
     {
-        cout<<"The number is a armstrong number"<<endl;
+        cout << "The number is a armstrong number" << endl;
     }
     else
     {
-        cout<<"The number is not a armstrong number"<<endl;
+        cout << "The number is not a armstrong number" << endl;
     }
-    
+
     return 0;
-    
 }
